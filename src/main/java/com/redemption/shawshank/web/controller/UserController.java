9a2df41f9167cb0083572975a0ec13f9b97698cd
xingshukui <xingshukui@163.com>
2017-07-04 17:11:17 +0800

    some problem

--- src/main/java/com/redemption/shawshank/web/controller/UserController.java
+++ src/main/java/com/redemption/shawshank/web/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.redemption.shawshank.web.controller;
 
 import com.redemption.shawshank.dao.UserMapper;
+import com.redemption.shawshank.pojo.UserExample;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,8 +26,11 @@ public class UserController {
     @RequestMapping("/1")
     public String u(){
 
+        UserExample example = new UserExample();
+        example.createCriteria().andIdEqualTo(1);
+
         logger.info("查询user信息开始.....");
-        return userMapper.selectByPrimaryKey(1).toString();
+        return userMapper.selectByExample(example).get(0).toString();
     }
 
 }
